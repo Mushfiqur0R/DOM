8aa456d56f920229bf4c6943ff16186207ee89c5
Mushfiqur0R <musfikur.dev@gmail.com>
2024-02-12 13:50:06 +0600

    added basic of DOM

---
+++ dom basic/js/append.js
@@ -0,0 +1,8 @@
+//1. where to add
+const placeList = document.getElementById('places-list');
+//2. what to be added
+const li = document.createElement('li');
+li.innerText = 'pahar toli';
+
+//3. add the chil
+placeList.appendChild(li);
---
+++ js dom event/js/event.js
@@ -0,0 +1,44 @@
+console.log('This is separate Js file');
+//option 1: directly set on the HTML element
+// <button onclick = "console.log(65)"> Another Button </button>
+
+//option 2: add one click function on the html element
+//IMPORTANT: We will use this
+//<button onclick="makeRed()">Make Red</button>
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+//option 3: 
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+//option 3 another:
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
+
+// option 4: 
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink)
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option 4: another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+});
+
+//option 4: Final
+// we will use this
+document.getElementById('make-gray').addEventListener('click', function makeGray() {
+    document.body.style.backgroundColor = 'gray';
+});
